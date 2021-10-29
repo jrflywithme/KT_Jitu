c9caa50dadf3032c0fff86e231efb7dfb83af29b
Jitendra <Jitendra.r@logicserve.com>
2021-10-29 20:16:23 +0530

    first commit

---
+++ Controller/UploadController.cs
@@ -0,0 +1,70 @@
+﻿using blalogin.Data;
+using ExcelDataReader;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace blalogin.Controller
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UploadController : ControllerBase
+    {
+        private readonly IWebHostEnvironment environment;
+        public UploadController(IWebHostEnvironment environment)
+        {
+            this.environment = environment;
+        }
+
+
+        [HttpPost]
+        public async Task Post()
+        {
+            if (HttpContext.Request.Form.Files.Any())
+            {
+                foreach (var file in HttpContext.Request.Form.Files)
+                {
+                    var path = Path.Combine(environment.ContentRootPath, "uploads", file.FileName);
+                    using (var stream = new FileStream(path, FileMode.Create))
+                    {
+                        await file.CopyToAsync(stream);
+                    }
+                }
+            }
+        }
+
+       
+        // [HttpPost]
+        public void ReadExcelFile(string fileName)
+        {
+            List<Route> routs = new List<Route>();
+            //var fileName = "./Users.xlsx";
+            // For .net core, the next line requires the NuGet package, 
+            // System.Text.Encoding.CodePages
+            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+            using (var stream = System.IO.File.Open(fileName, FileMode.Open, FileAccess.Read))
+            {
+                using (var reader = ExcelReaderFactory.CreateReader(stream))
+                {
+
+                    while (reader.Read()) //Each row of the file
+                    {
+                        routs.Add(new Route
+                        {
+                            Id = Convert.ToInt32(reader.GetValue(0)),
+                            Routename = reader.GetValue(1).ToString(),
+                            Supervisorname = reader.GetValue(2).ToString(),
+                            Billdays = Convert.ToInt32(reader.GetValue(3)),
+                            Code = Convert.ToInt32(reader.GetValue(4))
+                        });
+                    }
+                }
+            }
+            //return View(users);
+        }
+    }
+}
---
+++ Data/RouteService.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+
+namespace blalogin.Data
+{
+    public class RouteService
+    {
+        #region Property
+        private readonly ApplicationDbContext _appDBContext;
+        #endregion
+
+        #region Constructor
+        public RouteService(ApplicationDbContext appDBContext)
+        {
+            _appDBContext = appDBContext;
+        }
+        #endregion
+
+        #region Get List of Routes
+        public async Task<List<Route>> GetAllRoutesAsync(int currentPage,int pageSize)
+        {
+            return await _appDBContext.Routes.OrderByDescending(x=>x.Id).Skip((currentPage-1)*pageSize).Take(pageSize).ToListAsync();
+        }
+        #endregion
+
+        public async Task<int> GetCountAsync()
+        {
+            return await _appDBContext.Routes.CountAsync();
+        }
+        
+
+        #region Insert Route
+        public async Task<bool> InsertRouteAsync(Route route)
+        {
+            await _appDBContext.Routes.AddAsync(route);
+            await _appDBContext.SaveChangesAsync();
+            return true;
+        }
+        #endregion
+
+        #region Get Route by Id
+        public async Task<Route> GetRouteAsync(int Id)
+        {
+            Route route = await _appDBContext.Routes.FirstOrDefaultAsync(c => c.Id.Equals(Id));
+            return route;
+        }
+        #endregion
+
+         
+        #region Get Route by Name
+        public async Task<List<Route>> SearchRoutesAsync(string searchItem)
+        {
+            return await _appDBContext.Routes.Where(s => s.Routename.ToLower().Contains(searchItem.ToLower())).ToListAsync();
+        }
+        #endregion
+
+        #region Update Route
+        public async Task<bool> UpdateRouteAsync(Route route)
+        {
+             _appDBContext.Routes.Update(route);
+            await _appDBContext.SaveChangesAsync();
+            return true;
+        }
+        #endregion
+
+        #region Deleteroute
+        public async Task<bool> DeleteRouteAsync(Route route)
+        {
+            _appDBContext.Remove(route);
+            await _appDBContext.SaveChangesAsync();
+            return true;
+        }
+        #endregion
+ 
+        public async Task<List<Route>> SearchIssuesAsync(string searchItem)
+        {
+            return await _appDBContext.Routes.Where(s => s.Routename.ToLower().Contains(searchItem.ToLower())).ToListAsync();
+        }
+        public async Task<bool> SaveRoutes(List<Route> routes) {
+            try
+            {
+                foreach (var item in routes)
+                {
+                    _appDBContext.Routes.Add(item);
+                }
+                await _appDBContext.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+           
+        
+        }
+
+
+
+    }
+}
+ 
